999214ddd202399a1f3f36bdb82e0d179350d699
Achraf Chaibi <chaibi.achraf123@gmail.com>
2026-03-31 23:47:42 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-slate-950">
+      <p className="text-5xl font-semibold tracking-tight text-white">Hello</p>
+    </div>
+  );
+}
+
+export default App;
